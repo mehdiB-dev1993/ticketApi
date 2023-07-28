a7fb104fadff23d74511e8bef1b67b7fcb6e9cc1
mehdiBakhshi <mehdi.1993.dev@gmail.com>
2023-07-29 03:27:59 +0330

    edit-1

--- App/Models/TicketModel.php
+++ App/Models/TicketModel.php
@@ -7,34 +7,40 @@ use App\Core\DB;
 class TicketModel
 
 {
+    protected $db;
+
+    public function __construct()
+    {
+        $this->db = new DB();
+    }
+
     public function GetList()
     {
-        $db = new DB();
+
 
         $query = "select * from tickets ";
-        return $db->Read($query);
+        return $this->db->Read($query);
     }
 
     public function CreateTicket($data)
     {
-        $db = new DB();
 
-        $LastID = $db->Create('tickets',$data);
+        $LastID = $this->db->Create('tickets',$data);
         $data = array('userID' => 0 ,'ticketID' => $LastID);
-        $db->Create('user_ticket',$data);
+        $this->db->Create('user_ticket',$data);
         return 'created!';
     }
 
     public function UpdateTicket($data,$condition)
     {
-        $db = new DB();
-        $result = $db->Update('tickets',$data,$condition);
+
+        $result = $this->db->Update('tickets',$data,$condition);
         return $result;
     }
     public function DeleteTicket()
     {
-        $db = new DB();
-        $result = $db->Delete('tickets');
+
+        $result = $this->db->Delete('tickets');
         return $result;
     }
 }
